aadf2635085997cc009351b74b383b4a8c485a38
AAKASH CHANDRA V <109978365+aakashchandra2002@users.noreply.github.com>
2024-03-16 11:52:53 +0530

    response to education in .js

--- JS/main.js
+++ JS/main.js
@@ -48,9 +48,9 @@ ScrollReveal({
 });
 
 ScrollReveal().reveal('.home-content, heading', { origin: 'top' });
-ScrollReveal().reveal('.home-img, .expertise-container, .portfolio-box, .contact-form', { origin: 'buttom' });
+ScrollReveal().reveal('.home-img, .expertise-container, .portfolio-box, .contact form', { origin: 'bottom' });
 ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
-ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+ScrollReveal().reveal('.home-content p, .about-content,.timeline', { origin: 'right' });
 
 /*================== typed js ==============*/
 
